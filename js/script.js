97e1e40ffaf420fccfd9a9a21fa046826904abd8
leychenkoMaxim <max242009@ukr.net>
2022-11-29 10:19:45 +0200

    123

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+  /*  */
+  alert('Не судите строго!');
+// ============================Змінні========================================
+let userEyesColor;
+
+let user;
+let userName = 'Вася';
+console.log(userName);
+
+user = userName;
+console.log(user);
+
+if(true){
+	var size = 15;
+}
+console.log(size);
+
+
+// ===========================Типи данних=========================================
+
+
+
